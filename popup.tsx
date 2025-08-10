76e8a5d527e3bc203fee3a4da152fdb74d27189f
almog1her <almog1her@gmail.com>
2025-08-10 17:04:36 +0300

    Fix import order for styles in popup.tsx and update icon.png

--- popup.tsx
+++ popup.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './src/App'
-import './src/styles/tailwind.css'
 import './src/styles/globals.css'
+import './src/styles/tailwind.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
